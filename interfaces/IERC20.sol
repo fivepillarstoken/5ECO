9665d15f4366f1603d8c80fc204091ecde59d46d
5PT <support@five-pillars.tech>
2026-07-07 21:39:26 +0200

    Delete interfaces/IERC20.sol

--- interfaces/IERC20.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-import {IERC20Metadata as OpenZeppelinIERC20} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-
-interface IERC20 is OpenZeppelinIERC20 {}
